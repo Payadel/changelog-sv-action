6a0cb91b3dbb1f0dc58e04f52d54a08546d9e26c
Hamid Molareza <kitm.hamid@gmail.com>
2023-04-26 15:26:44 +0330

    style: update `tabWidth` style

--- src/main.ts
+++ src/main.ts
@@ -5,70 +5,76 @@ import * as fs from "fs";
 const CHANGELOG_FILE_NAME = "CHANGELOG.md";
 
 async function main(): Promise<void> {
-  const inputVersion = core.getInput("version");
+    const inputVersion = core.getInput("version");
 
-  //Install standard-version package
-  let execOutput = await exec.getExecOutput("npm install -g standard-version");
-  if (execOutput.exitCode !== 0) {
-    core.setFailed(
-      `Install standard-version npm package failed: ${execOutput.stderr}`
+    //Install standard-version package
+    let execOutput = await exec.getExecOutput(
+        "npm install -g standard-version"
     );
-    return;
-  }
-
-  // If specific version is requested, update version:
-  if (inputVersion) {
-    core.info(`set version to ${inputVersion}`);
-    execOutput = await updateVersion(inputVersion);
     if (execOutput.exitCode !== 0) {
-      core.setFailed(
-        `Update version to requested version failed: ${execOutput.stderr}`
-      );
-      return;
+        core.setFailed(
+            `Install standard-version npm package failed: ${execOutput.stderr}`
+        );
+        return;
     }
-  }
 
-  let changelogCommand = "standard-version --skip.tag --skip.commit";
-  if (inputVersion) changelogCommand += " --skip.bump";
+    // If specific version is requested, update version:
+    if (inputVersion) {
+        core.info(`set version to ${inputVersion}`);
+        execOutput = await updateVersion(inputVersion);
+        if (execOutput.exitCode !== 0) {
+            core.setFailed(
+                `Update version to requested version failed: ${execOutput.stderr}`
+            );
+            return;
+        }
+    }
 
-  try {
-    await exec
-      .getExecOutput(changelogCommand)
-      .then(() => exec.getExecOutput(`git add ${CHANGELOG_FILE_NAME}`))
-      //set output 'version'
-      .then(() =>
-        getVersion(inputVersion).then(version =>
-          core.setOutput("version", version)
-        )
-      )
-      // The version must not be changed until release.
-      .then(() =>
-        exec.getExecOutput("git checkout -- package.json package-lock.json")
-      )
-      //set output 'changelog'
-      .then(() =>
-        core.setOutput(
-          "changelog",
-          fs.readFileSync(CHANGELOG_FILE_NAME, "utf8")
-        )
-      );
-  } catch (error: any) {
-    core.setFailed(error instanceof Error ? error.message : error.toString());
-  }
+    let changelogCommand = "standard-version --skip.tag --skip.commit";
+    if (inputVersion) changelogCommand += " --skip.bump";
+
+    try {
+        await exec
+            .getExecOutput(changelogCommand)
+            .then(() => exec.getExecOutput(`git add ${CHANGELOG_FILE_NAME}`))
+            //set output 'version'
+            .then(() =>
+                getVersion(inputVersion).then(version =>
+                    core.setOutput("version", version)
+                )
+            )
+            // The version must not be changed until release.
+            .then(() =>
+                exec.getExecOutput(
+                    "git checkout -- package.json package-lock.json"
+                )
+            )
+            //set output 'changelog'
+            .then(() =>
+                core.setOutput(
+                    "changelog",
+                    fs.readFileSync(CHANGELOG_FILE_NAME, "utf8")
+                )
+            );
+    } catch (error: any) {
+        core.setFailed(
+            error instanceof Error ? error.message : error.toString()
+        );
+    }
 }
 
 async function updateVersion(version: string): Promise<exec.ExecOutput> {
-  return exec.getExecOutput(
-    `standard-version --skip.changelog --skip.tag --skip.commit --release-as ${version}`
-  );
+    return exec.getExecOutput(
+        `standard-version --skip.changelog --skip.tag --skip.commit --release-as ${version}`
+    );
 }
 
 async function getVersion(version: string): Promise<string> {
-  if (version) return version;
+    if (version) return version;
 
-  return await exec
-    .getExecOutput("node -p -e \"require('./package.json').version\"")
-    .then(result => result.stdout.trim());
+    return await exec
+        .getExecOutput("node -p -e \"require('./package.json').version\"")
+        .then(result => result.stdout.trim());
 }
 
 main();
